c8eb5bff2e036aed7a8fe42dd17bf04876a7b95a
rizkimaulana32 <sijond34@gmail.com>
2026-09-10 20:23:56 +0700

    fix: update ApiService

--- app/src/main/java/com/example/tvapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/tvapp/data/remote/retrofit/ApiService.kt
@@ -1,16 +1,19 @@
 package com.example.tvapp.data.remote.retrofit
 
+import com.example.tvapp.data.remote.dto.ListTVShowsResponseItem
+import com.example.tvapp.data.remote.dto.TVShowDetailResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("shows")
     suspend fun getTVShows(
         @Query("page") page: Int = 0
-    )
+    ): List<ListTVShowsResponseItem>
 
     @GET("shows/{id}")
     suspend fun getTVShowDetail(
-        @Query("id") id: Int = 0
-    )
+        @Path("id") id: Int = 0
+    ): TVShowDetailResponse
 }
